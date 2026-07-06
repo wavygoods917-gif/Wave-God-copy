ae92c0514f42878ee2444879b1820d1bc78c2e0e
wavygoods917-gif <wavygoods917@gmail.com>
2026-07-06 16:24:16 -0400

    Add pages/_app.js

---
+++ pages/_app.js
@@ -0,0 +1,10 @@
+import '../styles/globals.css'
+import { useEffect } from 'react'
+
+function MyApp({ Component, pageProps }) {
+  useEffect(()=>{
+  },[])
+  return <Component {...pageProps} />
+}
+
+export default MyApp
